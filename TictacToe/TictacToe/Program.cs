21a7a3035bd98d4ac7b355f0d41573e704927390
Zane Zelenina <zane.zelenina@cgi.com>
2021-10-05 23:15:59 +0300

    Added initial logic for making turn.

--- TictacToe/TictacToe/Program.cs
+++ TictacToe/TictacToe/Program.cs
@@ -6,8 +6,23 @@ namespace TictacToe
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Let's play Tic Tac Toe.");
+
             Board board = new Board();
-            board.PrintBoard();
+            Turn turn = new Turn();
+            
+            int turns = 0;
+            int maxTurns = 9;
+            
+            while (turns <= maxTurns) {
+                // TODO: exit if someone won faster
+
+                board.PrintBoard();
+                turn.MakeTurn();
+
+                turns++;
+            };
+
         }
     }
 }
---
+++ TictacToe/TictacToe/Turn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TictacToe
+{
+    class Turn
+    {
+        public void MakeTurn() {
+            Console.WriteLine("Player 1 choose your field!");
+
+            try
+            {
+                string inputText = Console.ReadLine();
+                int playerInput = int.Parse(inputText);
+
+                if (playerInput < 1 || playerInput > 9)
+                {
+                    throw new ArgumentException("Number is not in allowed range");
+                };
+
+                Console.WriteLine($"Player entered: {playerInput}");
+            }
+            catch
+            {
+                Console.WriteLine("Please enter number in range from 1 to 9!");
+            }
+        }
+    }
+}
