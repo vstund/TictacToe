125ee246f1b4631886f29351b21d378445290b8b
Valērijs Stundiņš <vstund@gmail.com>
2021-10-17 19:11:43 +0300

    added control: validate input must be number in range 1-9

--- TictacToe/TictacToe/Turn.cs
+++ TictacToe/TictacToe/Turn.cs
@@ -9,11 +9,34 @@ namespace TictacToe
     class Turn
     {
         int input;
+        string inputTxt;
 
         public int GetInput(Player player)
         {
             Console.WriteLine($"{player.Name} choose your field! You are playing with '{player.Sign}'.");
-            input = Convert.ToInt32(Console.ReadLine());
+
+            input = 0;
+
+            while (input == 0)
+            {
+                inputTxt = Console.ReadLine();
+
+                switch (inputTxt)
+                {
+                    case "1": input = 1; break;
+                    case "2": input = 2; break;
+                    case "3": input = 3; break;
+                    case "4": input = 4; break;
+                    case "5": input = 5; break;
+                    case "6": input = 6; break;
+                    case "7": input = 7; break;
+                    case "8": input = 8; break;
+                    case "9": input = 9; break;
+                    default:
+                        Console.WriteLine("Validate input must be number in range 1-9."); break;
+                }
+            }
+            //input = Convert.ToInt32(Console.ReadLine());
 
             // TODO: validate input must be number in range 1-9
             // Console.WriteLine("Please enter number in range from 1 to 9!");
