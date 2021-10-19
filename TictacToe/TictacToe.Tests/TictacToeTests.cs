17ee02c9ae12428d2199c4df30176c85032c7028
Zane Zelenina <zane.zelenina@cgi.com>
2021-10-19 20:52:41 +0300

    Added base project for tests.

---
+++ TictacToe/TictacToe.Tests/TictacToeTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace TictacToe.Tests
+{
+    public class TictacToeTests
+    {
+        /// <summary>
+        /// Tests:
+        ///  - for winners logic
+        /// </summary>
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
