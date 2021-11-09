db41e079b529ba34a0e471db99bc78aaf15cb1aa
Valērijs Stundiņš <vstund@gmail.com>
2021-11-09 20:48:17 +0200

    Corrected

--- TictacToe/TictacToe/ComputerPlayer.cs
+++ TictacToe/TictacToe/ComputerPlayer.cs
@@ -8,7 +8,7 @@ namespace TictacToe
         public ComputerPlayer(Signs sign)
         {
             string[] computerPlayerName = new string[10] {"Charlie","Anna","Dzidra","Elizabeth","Leon","John","Vladimir","Aleksandr","Aigars","Teo" };
-            Name = computerPlayerName[new Random().Next(0, 9)];
+            Name = computerPlayerName[new Random().Next(0, computerPlayerName.Length-1)];
             Sign = sign;
         }
 
