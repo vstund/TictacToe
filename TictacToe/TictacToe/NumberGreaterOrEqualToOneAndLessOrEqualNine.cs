825127da4dfead083863f96e92ca648fc080daa1
Valērijs Stundiņš <vstund@gmail.com>
2021-11-09 21:48:58 +0200

    Added exception

---
+++ TictacToe/TictacToe/NumberGreaterOrEqualToOneAndLessOrEqualNine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TictacToe
+{
+    public class NumberGreaterOrEqualToOneAndLessOrEqualNine : Exception
+    {
+        public NumberGreaterOrEqualToOneAndLessOrEqualNine()
+        {
+
+        }
+        public NumberGreaterOrEqualToOneAndLessOrEqualNine(string message) : base(message)
+        {
+
+        }
+
+        public NumberGreaterOrEqualToOneAndLessOrEqualNine(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
